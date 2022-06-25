cfb202febd7a7f02fb68833d0fa96204539ddb89
Adam Hintz <109281@student.san.edu.pl>
2022-06-25 21:56:37 +0200

    V.05

---
+++ Client/Pages/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Pages
+{
+    public class ToDoItem
+    {
+
+
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+
+
+
+
+
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -19,7 +20,7 @@ namespace Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            
+            builder.Services.AddBlazoredLocalStorage();
             await builder.Build().RunAsync();
         }
     }
