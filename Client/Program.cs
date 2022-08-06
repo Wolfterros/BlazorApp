e9cf95811553240fea89bbbe224ffb57ba5a9d67
Adam Hintz <109281@student.san.edu.pl>
2022-08-06 22:11:50 +0200

    v1.2 test

--- Client/Program.cs
+++ Client/Program.cs
@@ -20,7 +20,7 @@ namespace Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddBlazoredLocalStorage();        
             await builder.Build().RunAsync();
         }
     }
